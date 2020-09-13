b9615360d8a4e55cc44e99baf3add02430ee512d
Redemptionc <842391412@qq.com>
2020-09-13 22:21:54 +0800

    一些小问题： 寻找路径中最后一个元素时，如果路径中没有/，那么路径就是文件名 ...

--- user/find.c
+++ user/find.c
@@ -6,26 +6,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
-// 注意，这个返回的是一个定长的字符数组，所以如果实际长度不足，后面有一段是空白，如果输出这个或者用这个作比较，是不符合要求
-char*
-fmtname(char *path)
-{
-  // 这个函数的作用就是从一个文件的完整路径中取出文件名
-  // 比如: ./hello.c -> hello.c
-  static char buf[DIRSIZ+1];
-  char *p;
 
-  // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++; 
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-  return buf;
-}
 // 返回path中最后一个斜杠之后的元素
 char *getLastElem(char *p){
     char *t=p;
@@ -35,6 +16,10 @@ char *getLastElem(char *p){
             last=t;
         }
         t++;
+    }
+    // 也可能没有/，那么p指向的文件名
+    if(last==0){
+        return p;
     }
     return last+1;
 }
@@ -69,25 +54,22 @@ void find(char* path,char *name){
         }
         strcpy(buf, path);
         p = buf+strlen(buf);
-        // 这里不用管原来的path有没有/，对于ls来说，有几个都一样
         *p++ = '/';
         while(read(fd, &de, sizeof(de)) == sizeof(de)){
             if(de.inum == 0)
                 continue;
             // 计算出当前dirent的路径
-            memmove(p, de.name, DIRSIZ);
-            p[DIRSIZ] = 0;
+            int t=strlen(de.name)>DIRSIZ?DIRSIZ:strlen(de.name);
+            memmove(p, de.name, t);
+            p[t] = 0;
+            // p让buf这个字符串现在是当前dirent的完整路径
             if(stat(buf, &st) < 0){
                 printf("ls: cannot stat %s\n", buf);
                 continue;
             }
-            // printf("p: %s\tde.name:%s\tbuf:%s\tfmtbuf:%s\n",p,de.name,buf,fmtname(buf));
             if(strcmp(de.name,".")==0||strcmp(de.name,"..")==0){
                 continue;
             }
-            // if(strcmp(de.name,name)==0){
-            //     printf("%s/%s\n",path,name);
-            // }
             find(buf,name);
         }
         break;
